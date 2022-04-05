04bf63b6f7a1eaeafca5500c2bf4db37cf858668
nurafansyarifudin <68774609+Afansyarifudin@users.noreply.github.com>
2022-04-05 11:40:54 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('auth.login');
 });
+
+Route::get('/dahsboard', function () {
+    return view('home.index');
+});
