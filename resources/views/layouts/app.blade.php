b864829b0363af03684999dc0026694209b408fd
Fidyastya <fidyastya932@gmail.com>
2022-04-11 12:07:28 +0700

    landing page Data Arsip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,36 +8,217 @@
 
     <link rel="shortcut icon" href="{{ url('assets/images/logo/favicon.png') }}">
     <link href="{{ url('assets/css/app.min.css') }}" rel="stylesheet">
+    <link href="{{ url('assets/vendors/datatables/dataTables.bootstrap.min.css') }}" rel="stylesheet">
+
+
 
     @stack('css')
 
 </head>
 
 <body>
-    <div class="app">
-        <div class="container-fluid p-h-0 p-v-20 bg full-height d-flex" style="background-image: url('assets/images/others/login-3.png')">
-            <div class="d-flex flex-column justify-content-between w-100">
-                <div class="container d-flex h-100">
-                    @yield('content')
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-primary navbar-dark fixed-top scrolling-navbar">
+        <div class="container">
+            <!-- Navbar brand -->
+            <a class="navbar-brand" href="#">
+                <img alt="logoarsip" src="{{ url('assets/images/logoarsip.png') }}" height="50"/>
+            </a>
+
+            <!-- Collapse button -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Collapsible content -->
+            <div class="collapse navbar-collapse" id="basicExampleNav">
+                <!-- Links -->
+                <ul class="navbar-nav mr-auto smooth-scroll"></ul>
+                </ul>
+                <!-- Links -->
+                <ul class="navbar-nav nav-flex-icons">
+                    <!-- Collapsible content -->
+                    <div class="collapse navbar-collapse" id="basicExampleNav">
+                        <!-- Links -->
+                        <ul class="navbar-nav mr-auto smooth-scroll" style="font-size: 20px">
+                            <li class="nav-item active px-3">
+                                <a class="nav-link" href="#">Beranda</a>
+                            </li>
+                            <li class="nav-item active px-3">
+                                <a class="nav-link" href="#">Arsip</a>
+                            </li>
+                            <li class="nav-item active px-3">
+                                <a class="nav-link" href="#">Login</a>
+                            </li>
+
+                            <!-- Dropdown -->
+                            {{--  <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                                <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>  --}}
+                        </ul>
+                        <!-- Links -->
+                    </div>
+                    <!-- Collapsible content -->
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- End Navbar -->
+
+    <!-- Content -->
+    <header class="masthead " >
+        <h1 class="mb-5">Arsip</h1>
+        <div class="d-flex align-items-center flex-column">
+            <div class="row">
+                <div class="col-lg-2 mb-3">
+                    <!-- Accordion -->
+                    <div class="accordion" id="accordion-default">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title">
+                                    <a data-toggle="collapse" href="#collapseOneDefault">
+                                        Arsip Terbaru
+                                    </a>
+                                </h5>
+                            </div>
+                            <div id="collapseOneDefault" class="collapse show" data-parent="#accordion-default">
+                                <div class="card-body">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title">
+                                    <a class="collapsed" data-toggle="collapse" href="#collapseTwoDefault">
+                                        Jurusan
+                                    </a>
+                                </h5>
+                            </div>
+                            <div id="collapseTwoDefault" class="collapse" data-parent="#accordion-default">
+                                <div class="card-body">
+                                    ...
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="d-none d-md-flex p-h-40 justify-content-between">
-                    <span class="">© 2022 FMIPA UNNES</span>
-                    <ul class="list-inline">
-                        <li class="list-inline-item">
-                            <a class="text-dark text-link" href="">Legal</a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="text-dark text-link" href="">Privacy</a>
-                        </li>
-                    </ul>
+
+                <div class="col-lg-10">
+                    <div class="container-fluid">    
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-8">
+                                            <h4><b>DATA ARSIP</b></h4>
+                                            <p>Di bawah ini merupakan list daftar arsip surat yang tersedia dalam sistem</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <table class="table" id="datatable">
+                                        <thead>
+                                            <th>No</th>
+                                            <th>Judul</th>
+                                            <th>No Surat Lengkap</th>
+                                            <th>Rak Arsip</th>
+                                            <th>Unit Arsip</th>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Lorem ipsum dolor sit amet</td>
+                                                <td>Lorem ipsum dolor sit amet</td>
+                                                <td>Lorem ipsum dolor sit amet</td>
+                                                <td>Lorem ipsum dolor sit amet</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        {{--  <div class="card shadow">
+                            <div class="card-header py-3">
+                                <p class="text-primary m-0 font-weight-bold">DATA ARSIP</p>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-3"></div>
+                                    <div class="col-md-6"></div>
+                                    <div class="col-md-3">
+                                        <select class="combobox">
+                                            <option value="Select">Penelitian Dosen</option>
+                                            <option value="php">SK Kenaikan Pangkat</option>
+                                            <option value="html">Surat-Surat</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <!-- Tabel -->
+                                <table class="table" id="datatable">
+                                    <thead>
+                                        <th>No</th>
+                                        <th>Judul</th>
+                                        <th>No Surat Lengkap</th>
+                                        <th>Rak Arsip</th>
+                                        <th>Unit Arsip</th>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Lorem ipsum dolor sit amet</td>
+                                            <td>Lorem ipsum dolor sit amet</td>
+                                            <td>Lorem ipsum dolor sit amet</td>
+                                            <td>Lorem ipsum dolor sit amet</td>
+                                        </tr>
+                                </tbody>
+                                </table>
+                                <div class="table-responsive mt-2" id="datatable">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                        <thead>
+                                            <tr class="text-center">
+                                                <th>No</th>
+                                                <th>Judul</th>
+                                                <th>No Surat</th>
+                                                <th>Rak Arsip</th>
+                                                <th>Unit Arsip</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>Lorem ipsum dolor sit amet</td>
+                                                    <td>Lorem ipsum dolor sit amet</td>
+                                                    <td>Lorem ipsum dolor sit amet</td>
+                                                    <td>Lorem ipsum dolor sit amet</td>
+                                                </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>  --}}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-
+    </header>
+    <!-- End Navbar -->
 
     <script src="{{ url('assets/js/vendors.min.js') }}"></script>
     <script src="{{ url('assets/js/app.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="{{ url('assets/vendors/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ url('assets/vendors/datatables/dataTables.bootstrap.min.js') }}"></script>
 
     @stack('js')
 
