0b644a2236aa43ddff1bf3c78549f225a698957c
Fidyastya <fidyastya932@gmail.com>
2022-04-11 21:52:46 +0700

    Index, create, edit, show surat masuk

---
+++ resources/views/surat_masuk/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.master')
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col-md-8">
+                        <h4><b>Edit Surat Masuk</b></h4>
+                        <p>Mengedit Daftar Surat Masuk</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <form>
+                    <div class="form-row mb-3">
+                        <div class="col">
+                            <label for="NomorSurat">Nomor Surat</label>
+                            <input type="text" class="form-control" id="NomorSurat" placeholder="Nomor Surat">
+                        </div>
+                        <div class="col"> 
+                            <label for="SifatSurat">Sifat Surat</label>
+                            <select class="custom-select">
+                                <option value="1">Sangat Segera atau Kilat</option>
+                                <option value="2">Segera</option>
+                                <option value="3">Dinas</option>
+                                <option value="4">Biasa</option>
+                              </select>                              
+                        </div>
+                    </div>
+                    <div class="form-row mb-3">
+                        <div class="col">
+                            <label for="PerihalSurat">Perihal Surat</label>
+                            <input type="text" class="form-control" id="PerihalSurat" placeholder="Perihal Surat">
+                        </div>
+                        <div class="col">
+                            <label for="TujuanSurat">Tujuan Surat</label>
+                            <input type="text" class="form-control" id="TujuanSurat" placeholder="Tujuan Surat">
+                        </div>
+                    </div>
+                    <div class="form-row mb-3">
+                        <div class="col">
+                            <label for="TanggalMasuk">Tanggal Masuk</label>
+                            <input type="date" class="form-control" placeholder="Tanggal Masuk">
+                        </div>
+                        <div class="col">
+                            <label for="TanggalDiterima">Tanggal Diterima</label>
+                            <input type="date" class="form-control" placeholder="Tanggal Diterima">
+                        </div>
+                    </div>
+                    <a href="#" class="btn btn-primary float-right">Simpan</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/surat_masuk/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.master')
+@push('css')
+<link href="{{ url('assets/vendors/datatables/dataTables.bootstrap.min.css') }}" rel="stylesheet">
+@endpush
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col-md-8">
+                        <h4><b>Surat Masuk</b></h4>
+                        <p>Di bawah ini merupakan list surat masuk yang tersedia</p>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="#" class="btn btn-primary float-right">Tambah Surat Masuk</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <table class="table table-hover" id="datatable">
+                    <thead>
+                        <th>No</th>
+                        <th>Nomor Surat</th>
+                        <th>Sifat Surat</th>
+                        <th>Perihal Surat</th>
+                        <th>Tujuan Surat</th>
+                        <th style="width: 10px; text-align: center"><i class='anticon anticon-setting'></i></th>
+                    </thead>
+                    <tbody>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@push('js')
+<script>
+    $(document).ready( function () {
+        $('#datatable').DataTable();
+    } );
+</script>
+<script src="{{ url('assets/vendors/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ url('assets/vendors/datatables/dataTables.bootstrap.min.js') }}"></script>
+@endpush
