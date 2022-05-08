9936ab983416f92443dd7fbd748d5bf7abd816d7
NUr Afan Syarifudin <afan.syarifudin10@gmail.com>
2022-05-08 10:21:20 +0700

    UPDATE Head

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>E-Arsip FMIPA UNNES</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="assets/images/logo/favicon.png">
+    <!-- Icon Arsip -->
+    <link rel="shortcut icon" href="{{ url('assets/images/iconarsip.png') }}">
 
     <!-- page css -->
 
