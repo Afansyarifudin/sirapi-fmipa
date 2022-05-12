3e16b64acbf7aeac92545e3508bbfd99adf33bb6
NUr Afan Syarifudin <afan.syarifudin10@gmail.com>
2022-05-13 03:02:58 +0700

    ADD Arsip Landing Page

--- resources/views/guest/arsip.blade.php
+++ resources/views/guest/arsip.blade.php
@@ -75,15 +75,146 @@
     </nav>
 
     <!--Isi-->
-    <section id="Home" class="Home">
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        @yield('content')
-    </section>
+    <header class="masthead " >
+        <h1 class="mb-5">Arsip</h1>
+        <div class="d-flex align-items-center flex-column">
+            <div class="row">
+                <div class="col-lg-2 mb-3">
+                    <!-- Accordion -->
+                    <div class="accordion" id="accordion-default">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title">
+                                    <a data-toggle="collapse" href="#collapseOneDefault">
+                                        Arsip Terbaru
+                                    </a>
+                                </h5>
+                            </div>
+                            <div id="collapseOneDefault" class="collapse show" data-parent="#accordion-default">
+                                <div class="card-body">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title">
+                                    <a class="collapsed" data-toggle="collapse" href="#collapseTwoDefault">
+                                        Jurusan
+                                    </a>
+                                </h5>
+                            </div>
+                            <div id="collapseTwoDefault" class="collapse" data-parent="#accordion-default">
+                                <div class="card-body">
+                                    ...
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-10">
+                    <div class="container-fluid">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-8">
+                                            <h4><b>DATA ARSIP</b></h4>
+                                            <p>Di bawah ini merupakan list daftar arsip surat yang tersedia dalam sistem</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <table class="table" id="datatable">
+                                        <thead>
+                                            <th>No</th>
+                                            <th>Judul</th>
+                                            <th>No Surat Lengkap</th>
+                                            <th>Rak Arsip</th>
+                                            <th>Unit Arsip</th>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Lorem ipsum dolor sit amet</td>
+                                                <td>Lorem ipsum dolor sit amet</td>
+                                                <td>Lorem ipsum dolor sit amet</td>
+                                                <td>Lorem ipsum dolor sit amet</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        {{--  <div class="card shadow">
+                            <div class="card-header py-3">
+                                <p class="text-primary m-0 font-weight-bold">DATA ARSIP</p>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-3"></div>
+                                    <div class="col-md-6"></div>
+                                    <div class="col-md-3">
+                                        <select class="combobox">
+                                            <option value="Select">Penelitian Dosen</option>
+                                            <option value="php">SK Kenaikan Pangkat</option>
+                                            <option value="html">Surat-Surat</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <!-- Tabel -->
+                                <table class="table" id="datatable">
+                                    <thead>
+                                        <th>No</th>
+                                        <th>Judul</th>
+                                        <th>No Surat Lengkap</th>
+                                        <th>Rak Arsip</th>
+                                        <th>Unit Arsip</th>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Lorem ipsum dolor sit amet</td>
+                                            <td>Lorem ipsum dolor sit amet</td>
+                                            <td>Lorem ipsum dolor sit amet</td>
+                                            <td>Lorem ipsum dolor sit amet</td>
+                                        </tr>
+                                </tbody>
+                                </table>
+                                <div class="table-responsive mt-2" id="datatable">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                        <thead>
+                                            <tr class="text-center">
+                                                <th>No</th>
+                                                <th>Judul</th>
+                                                <th>No Surat</th>
+                                                <th>Rak Arsip</th>
+                                                <th>Unit Arsip</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>Lorem ipsum dolor sit amet</td>
+                                                    <td>Lorem ipsum dolor sit amet</td>
+                                                    <td>Lorem ipsum dolor sit amet</td>
+                                                    <td>Lorem ipsum dolor sit amet</td>
+                                                </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>  --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
     <script src="{{ url('assets/js/vendors.min.js') }}"></script>
     <script src="{{ url('assets/js/app.min.js') }}"></script>
