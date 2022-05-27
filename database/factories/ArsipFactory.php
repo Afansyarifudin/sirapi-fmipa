2e5133b48b737925b7ba387e366900d42a570b00
NUr Afan Syarifudin <afan.syarifudin10@gmail.com>
2022-05-27 10:40:36 +0700

    ADD factory

---
+++ database/factories/ArsipFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArsipFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'category_id' => 1,
+            'user_id' => 1,
+            'name'=> $this->faker->sentence(3),
+            'sifat'=> 'public',
+            'file' => 'jpg',
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Arsip;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Database\Seeders\RoleSeeder;
@@ -18,6 +19,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        Arsip::factory(100)->create();
 
 
         $this->call(CategorySeeder::class);
