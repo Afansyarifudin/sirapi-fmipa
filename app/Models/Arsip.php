596896154ba2fef31a9b4f2cfd9c2bb70992aa1a
NUr Afan Syarifudin <afan.syarifudin10@gmail.com>
2022-06-09 18:13:11 +0700

    RENEW Arsip model

---
+++ app/Models/Arsip.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Arsip extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'arsips';
+
+    protected $fillable = [
+        'category_id',
+        'user_id',
+        'name',
+        'sifat',
+        'file',
+
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+}
