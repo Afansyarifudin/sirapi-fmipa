6aae0ea85703149a3ead5576614e62b2f6125b98
iqabdillah <iqabdillah09@students.unnes.ac.id>
2022-06-10 17:31:00 +0700

    data arsip - edit sudah bisa dibuka tapi belum bisa filenya - delete sudah bisa tapi filenya belum kehapus - view belum  manajemen arsip - sudah bisa ditampilkan - delete sudah bisa tapi filenya belum kehapus - view belum  manajemen user - edit bisa dibuka tapi belum berhasil - delete amann

--- app/Http/Controllers/ManajemenArsipController.php
+++ app/Http/Controllers/ManajemenArsipController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Arsip;
+use Illuminate\Support\Facades\DB;
 
 class ManajemenArsipController extends Controller
 {
@@ -11,8 +13,28 @@ class ManajemenArsipController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $arsip_list = Arsip::all();
+
+        if($request->ajax()){
+            return datatables() -> of($arsip_list)
+            ->addColumn('action', function($data){
+
+
+                return "<div style='text-align: center'>
+                    <a href='' data-toggle='tooltip'  data-id='".$data->id."' data-original-title='View' class='badge badge-success mx-1'><i class='anticon anticon-select'></i></a>
+                    <a href=' ' class='badge badge-danger mx-1 deleteButton' data-form='#dataDeleteButton$data->id'> <i class='far fa-trash-alt'></i> </a>
+
+                    <form id='dataDeleteButton$data->id' action='" . route('arsip.destroy', $data->id) . "' method='POST'>" . csrf_field() . " " . method_field('DELETE') . "
+                    </form>
+                </div>";
+            })
+            ->rawColumns(['action'])
+            ->addIndexColumn()
+
+            -> make(true);
+        }
         return view('operator.arsip.index');
     }
 
@@ -79,6 +101,20 @@ class ManajemenArsipController extends Controller
      */
     public function destroy($id)
     {
-        //
+        {
+            try {
+                DB::beginTransaction();
+                $delete = Arsip::where('id', $id)->delete();
+                DB::commit();
+                return ($delete) ?
+                redirect()->route('arsip.index')->with('success', 'Data Arsip berhasil dihapus') :
+                back()->with('error', 'Data Arsip gagal dihapus');
+
+            } catch (\Exception $e) {
+                DB::rollBack();
+                return back()->with('error', 'terjadi kesalahan, Data Arsip gagal dihapus');
+
+            }
+        }
     }
 }
