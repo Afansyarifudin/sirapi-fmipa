b6666232efdc9ee65c6562e3d3de527c15bdee8f
Fidyastya <fidyastya932@gmail.com>
2022-06-17 11:19:06 +0700

    update view manajemen arsip

---
+++ resources/views/operator/arsip/show.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.master')
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col-md-8">
+                        <h4><b>Detail Data Manajemen Arsip</b></h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Nama</th>
+                            <td>{{ $data->name }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Sifat Arsip</th>
+                            <td>{{ $data->sifat }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Kategori</th>
+                            <td>{{ $data->category_id }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Created by</th>
+                            <td>{{ $data->user_id }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <embed src="/{{ $data->file }}" width="1020"  height="600" alt="pdf" />
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
